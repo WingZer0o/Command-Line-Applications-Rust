f50c9da2127ff5324b8dbbc74588fe70d18bd9b2
Mike Mulchrone <mtmulch0191@outlook.com>
2022-10-10 15:55:41 -0400

    init with command line argument acceptance.

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,13 @@
+#![allow(unused)]
+
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
 }
